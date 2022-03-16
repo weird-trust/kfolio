1c1f419b20066a85947e42a55b63cfede32d68a0
weird-trust <robert.burtzik@fork.de>
2022-03-16 10:24:33 +0100

    worked on slideshow component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const slides = document.getElementsByClassName('slides');
+  const slideshows = document.getElementsByClassName('slideshow');
+  for (const slideshow of slideshows) {
+    slideshow.firstElementChild.className = 'slides showing ';
+  }
   let currentSlide = 0;
-  slides[0].className = 'slides showing ';
+  const slides = document.getElementsByClassName('slides');
+  // slides[0].className = 'slides showing ';
   for (const slide of slides) {
     slide.addEventListener('click', (event) => {
+      event.preventDefault();
       slides[currentSlide].className = 'slides ';
       currentSlide = (currentSlide + 1) % slides.length;
       slides[currentSlide].className = 'slides showing';
--- www/site/snippets/slideshows.php
+++ www/site/snippets/slideshows.php
@@ -2,10 +2,8 @@
 <section id="<?= $slideshow->id() ?>" class="slideshow">
   <?php foreach($slideshow->images() as $image): ?>
   <div class="slides">
-
     <img src=" <?= $image->resize(null, 500)->url(); ?>" alt="<?=$image->name()?>"
       data-slide="{<?= $image->resize(null, 180)->url(); ?>}" />
-
   </div>
   <?php endforeach ?>
 </section>
