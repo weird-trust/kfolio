8d3159a203bdd9a6ed9182688657a3ef62adc09a
weird-trust <robert.burtzik@fork.de>
2022-04-24 13:09:26 +0200

    feat(slideshow) adjusted for loop and click events in multiple slideshows

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const slideshows = document.getElementsByClassName('slideshow');
+
   for (const slideshow of slideshows) {
+    const slides = slideshow.getElementsByClassName('slides');
     slideshow.firstElementChild.className = 'slides showing ';
-  }
-  let currentSlide = 0;
-  const slides = document.getElementsByClassName('slides');
-  // slides[0].className = 'slides showing ';
-  for (const slide of slides) {
-    slide.addEventListener('click', (event) => {
-      event.preventDefault();
-      slides[currentSlide].className = 'slides ';
-      currentSlide = (currentSlide + 1) % slides.length;
-      slides[currentSlide].className = 'slides showing';
-    });
+    let currentSlide = 0;
+
+    for (const slide of slides) {
+      slide.addEventListener('click', (event) => {
+        event.preventDefault();
+        slides[currentSlide].className = 'slides';
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].className = 'slides showing';
+      });
+    }
   }
 });
