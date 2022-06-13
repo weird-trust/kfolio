262ace3f3782ca46b2f3d9fb975748e78b8c10e7
weird-trust <robert.burtzik@fork.de>
2022-06-13 15:41:17 +0200

    feat(layout) toggle for layout shift

--- www/site/snippets/designproject.php
+++ www/site/snippets/designproject.php
@@ -1,6 +1,7 @@
-<section class="slideshow">
+<?php if($data->toogle()->bool() === false): ?>
+<section class="slideshow right">
   <?php foreach($data->images() as $image): ?>
-  <div class=" slides">
+  <div class="slides">
     <img class="lazy-loaded-image lazy" alt="<?= $image->alt() ?>" src="www/assets/images/placeholder.webp"
       data-src="<?= $image->resize(750)->url() ?>" />
   </div>
@@ -11,5 +12,21 @@
     </h2>
     <h3 class="info"> <?= $data->year() ?></h3>
   </div>
-
 </section>
+<?php endif ?>
+<?php if($data->toogle()->bool() === true): ?>
+<section class="slideshow left">
+  <?php foreach($data->images() as $image): ?>
+  <div class="slides">
+    <img class="lazy-loaded-image lazy" alt="<?= $image->alt() ?>" src="www/assets/images/placeholder.webp"
+      data-src="<?= $image->resize(750)->url() ?>" />
+  </div>
+  <?php endforeach ?>
+  <div class="txt-wrapper">
+    <h2 class="title">
+      <?= $data->title() ?>
+    </h2>
+    <h3 class="info"> <?= $data->year() ?></h3>
+  </div>
+</section>
+<?php endif ?>
