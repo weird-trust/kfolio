d6b1fc98cfc4a1b26d08c66336d30a57cb2b59c3
weird-trust <robert.burtzik@fork.de>
2022-06-13 12:26:57 +0200

    feat(layout) adjusted srcset and blueprints

--- www/site/snippets/photoproject.php
+++ www/site/snippets/photoproject.php
@@ -2,7 +2,7 @@
   <ul class="photoproject">
     <li>
       <?php foreach($data->images() as $image): ?>
-      <img class="lazy-loaded-image lazy pixelator" alt="<?= $image->alt() ?>" src="www/assets/images/placeholder.webp"
+      <img class="lazy-loaded-image lazy pixelator" alt="<?= $image->alt() ?>" src="assets/images/placeholder.webp"
         data-src="<?= $image->resize(600)->url() ?>" />
       </a>
       <?php endforeach ?>
